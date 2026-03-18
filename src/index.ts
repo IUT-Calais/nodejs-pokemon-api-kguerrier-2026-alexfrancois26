ee0c92abce71c4a802c0dad2be9a086324be8f45
Alex François <alexfrancois2604@gmail.com>
2026-03-18 21:04:37 +0100

    ajout du swagger pour l'API-doc

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import { pokemonCardRouter } from './pokemonCard/pokemonCard.router';
 import { userRouter } from './user/user.router';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
 
 
 export const app = express();
@@ -8,10 +11,16 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// Configuration Swagger
+const swaggerDocument = YAML.load(path.join('.', 'swagger', 'swagger.yaml'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 // modification de l'export pour permettre l'arrêt du serveur dans les tests car sinon cela ne fonctionne pas et les tests ne se terminent jamais
 export let server: any;
 if (process.env.NODE_ENV !== 'test') {
   server = app.listen(port);
+  console.log(`🚀 Serveur lancé sur http://localhost:${port}`);
+  console.log(`📚 Documentation Swagger disponible sur http://localhost:${port}/api-docs`);
 }
 
 app.use('/pokemon-cards', pokemonCardRouter);
